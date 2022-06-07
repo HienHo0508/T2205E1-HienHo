48fdd406adaed6c53405496a2a1ed15da23f9efc
HienHo0508 <106592370+HienHo0508@users.noreply.github.com>
2022-06-07 17:54:36 +0700

    Add files via upload

---
+++ lab02-bai5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+float top;
+float bottom;
+float height;
+float s;
+
+
+printf("nhap day be\n");
+scanf("%f",&top);
+printf("nhap day lon\n");
+scanf("%f",&bottom);
+printf("chieu cao\n");
+scanf("%f",&height);
+s= (top + bottom) * height /2;
+printf(" dien tich hinh thang la  %f",s);
+
+}
+
+
