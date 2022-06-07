6d2529a0f59b3ae79337ef669af61cf47ce48002
HienHo0508 <106592370+HienHo0508@users.noreply.github.com>
2022-06-07 17:55:47 +0700

    Add files via upload

---
+++ lab02-bai7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main ()
+{float r;
+float pi =3.14;
+float c;
+printf("nhap ban kinh hinh tron\n");
+scanf("%f",&r);
+c = r * 2 * pi;
+printf("chu vi hinh tron la %f\n",c);
+
+}
