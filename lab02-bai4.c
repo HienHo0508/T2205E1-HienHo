bf48a3c9d1984dd9fe4d536e8872474af099831c
HienHo0508 <106592370+HienHo0508@users.noreply.github.com>
2022-06-07 17:54:19 +0700

    Add files via upload

---
+++ lab02-bai4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+float do_C;
+float do_F;
+
+printf("nhap do C\n");
+scanf("%f",&do_C);
+do_F= (do_C/5)* 9 +32;
+printf("Nhiet do F quy doi ra la   %f\n",do_F);
+
+}
+
+
